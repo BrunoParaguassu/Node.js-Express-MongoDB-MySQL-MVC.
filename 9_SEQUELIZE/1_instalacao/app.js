15603c7984a6244701b521ac97a8235d6a0f7f4b
Bruno Paraguassu Sousa <paraguassu.sousa@gmail.com>
2022-10-04 01:24:03 -0400

    Feat: SEQUELIZE

---
+++ 9_SEQUELIZE/1_instalacao/app.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const exphbs = require("express-handlebars");
+const conn = require("./db/conn");
+
+const app = express();
+
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
+
+app.use(express.json());
+
+app.engine("handlebars", exphbs.engine());
+app.set("view engine", "handlebars");
+
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  res.render("home");
+});
+
+ 
+app.listen(3000, () => {
+  console.log("Server running on port 3000");
+});
