3f8a1a0a7039747409fb1d8b2eeb52663e2def67
Bruno Paraguassu Sousa <paraguassu.sousa@gmail.com>
2022-10-05 01:39:22 -0400

    Feat:Sequelize

---
+++ 9_SEQUELIZE/5_resgatando_dados/db/conn.js
@@ -0,0 +1,15 @@
+const { Sequelize } = require('sequelize');
+
+const sequelize = new Sequelize('Sequelize','root','170614@Ba',{
+    host: 'localhost',
+    dialect: 'mysql',
+});
+
+/*try {
+    sequelize.authenticate();
+    console.log('Conectado com sucesso!');
+}catch(err){
+    console.log('Erro ao conectar!');
+}*/
+
+module.exports = sequelize;
