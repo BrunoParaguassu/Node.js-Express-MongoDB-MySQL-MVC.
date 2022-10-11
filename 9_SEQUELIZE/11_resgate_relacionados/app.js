f81bab6bb7021f61ad6763a3c810c539f0ea7e51
Bruno Paraguassu Sousa <paraguassu.sousa@gmail.com>
2022-10-11 11:10:14 -0400

    Feat: Sequelize.

---
+++ 9_SEQUELIZE/11_resgate_relacionados/app.js
@@ -0,0 +1,115 @@
+const { raw } = require("express");
+const express = require("express");
+const exphbs = require("express-handlebars");
+const conn = require("./db/conn");
+
+const User = require("./model/User");
+const Address = require("./model/Address");
+
+const app = express();
+
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
+
+app.use(express.json());
+
+app.engine("handlebars", exphbs.engine());
+app.set("view engine", "handlebars");
+
+app.use(express.static("public"));
+
+app.get("/users/create", (req, res) => {
+  res.render("adduser");
+});
+
+app.post("/users/create", async (req, res) => {
+  const name = req.body.name;
+  const occupation = req.body.occupation;
+  let newsletter = req.body.newsletter;
+
+  if (newsletter === "on") {
+    newsletter = true;
+  } else {
+    newsletter = false;
+  }
+
+  await User.create({ name, occupation, newsletter });
+  res.redirect("/");
+});
+
+app.get("/users/:id", async (req, res) => {
+  const id = req.params.id;
+
+  const user = await User.findOne({ raw: true, where: { id: id } });
+
+  res.render("userview", { user });
+});
+
+app.post("/users/delete/:id", async (req, res) => {
+  const id = req.params.id;
+
+  await User.destroy({ where: { id: id } });
+  res.redirect("/");
+});
+
+app.get("/users/edit/:id", async (req, res) => {
+  const id = req.params.id;
+
+  const user = await User.findOne({ include: Address, where: { id: id } });
+
+  res.render("useredit", { user: user.get({ plain: true }) });
+});
+
+app.post("/users/update/", async (req, res) => {
+  const id = req.body.id;
+  const name = req.body.name;
+  const occupation = req.body.occupation;
+  let newsletter = req.body.newsletter;
+
+  if (newsletter === "on") {
+    newsletter = true;
+  } else {
+    newsletter = false;
+  }
+
+  await User.update({ name, occupation, newsletter }, { where: { id: id } });
+  res.redirect("/");
+});
+
+app.get("/", async (req, res) => {
+  const users = await User.findAll({ raw: true });
+
+  console.log(users);
+
+  res.render("home", { users: users });
+});
+
+app.post("/address/create", async (req, res) => {
+  const UserId = req.body.UserId;
+  const street = req.body.street;
+  const number = req.body.number;
+  const city = req.body.city;
+
+  const address = {
+    street,
+    number,
+    city,
+    UserId,
+  };
+
+  await Address.create(address);
+
+  res.redirect(`/users/edit/${UserId}`);
+});
+
+conn
+  .sync()
+  //.sync({force: true})
+  .then(() => {
+    app.listen(3000, () => {
+      console.log("Servidor rodando!");
+    });
+  });
