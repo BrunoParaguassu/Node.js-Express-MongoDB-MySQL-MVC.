8e4c3221b50e737ec7403e54d9d475863fa81ed1
Bruno Paraguassu Sousa <paraguassu.sousa@gmail.com>
2022-10-22 21:19:08 -0400

    Feat: Criamos a rota de remover tasks, criamos a controller dessa rota onde a mesma dela pelo ID e redireciona para página de tasks, criamos um Form com botao para remover tasks e estilizamos a página.

---
+++ 10_MVC/8_deletando_dados/controllers/TaskController.js
@@ -0,0 +1,33 @@
+const Task = require("../models/task");
+
+module.exports = class TaskController {
+  static createTask(req, res) {
+    res.render("tasks/create");
+  }
+
+  static async createTaskSave(req, res) {
+    const task = {
+      title: req.body.title,
+      description: req.body.description,
+      done: false,
+    };
+    await Task.create(task);
+
+    res.redirect("/tasks");
+  }
+
+  static async removeTask(req, res) {
+
+    const id = req.body.id;
+
+    await Task.destroy({ where: { id: id } });
+
+    res.redirect("/tasks");
+  }
+
+  static async showTasks(req, res) {
+    const tasks = await Task.findAll({ raw: true });
+
+    res.render("tasks/all", { tasks });
+  }
+};
