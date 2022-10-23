7042d6bc24d17cef58792d7ad531c30bd7ece794
Bruno Paraguassu Sousa <paraguassu.sousa@gmail.com>
2022-10-22 22:29:47 -0400

    Feat: Criada a rota POST de update Status e sua controller para ficar visivel quando uma tarefa for concluida.

---
+++ 10_MVC/11_completando_tarefas/controllers/TaskController.js
@@ -0,0 +1,65 @@
+const Task = require("../models/task");
+
+module.exports = class TaskController {
+  static createTask(req, res) {
+    res.render("tasks/create");
+  }
+
+  static async createTaskSave(req, res) {
+    const task = {
+      title: req.body.title,
+      description: req.body.description,
+      done: false,
+    };
+    await Task.create(task);
+
+    res.redirect("/tasks");
+  }
+
+  static async removeTask(req, res) {
+    const id = req.body.id;
+
+    await Task.destroy({ where: { id: id } });
+
+    res.redirect("/tasks");
+  }
+
+  static async updateTask(req, res) {
+    const id = req.params.id;
+
+    const task = await Task.findOne({ where: { id: id }, raw: true });
+
+    res.render("tasks/edit", { task });
+  }
+
+  static async updateTaskPost(req, res) {
+    const id = req.body.id;
+
+    const task = {
+      title: req.body.title,
+      description: req.body.description,
+    };
+
+    await Task.update(task, { where: { id: id } });
+
+    res.redirect("/tasks");
+  }
+
+  static async toggleTaskStatus(req, res) {
+    const id = req.body.id;
+
+    const task = await Task.findOne({ where: { id: id }, raw: true });
+
+    task.done = !task.done;
+
+    await Task.update(task, { where: { id: id } });
+
+    res.redirect("/tasks");
+  }
+
+  static async showTasks(req, res) {
+    const tasks = await Task.findAll({ raw: true });
+
+    res.render("tasks/all", { tasks });
+  }
+};
