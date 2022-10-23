63b395f89c5bc8bf2997883a9139055c5ff1fc86
Bruno Paraguassu Sousa <paraguassu.sousa@gmail.com>
2022-10-22 21:03:22 -0400

    Feat: Criamos a controller showTasks, colocamos o link do bootstrap Icons para usarmos os icones no projeto, criamos links para esses icones, estilizamos a pagina showTasks.

--- 10_MVC/7_resgatando_dados/controllers/TaskController.js
+++ 10_MVC/7_resgatando_dados/controllers/TaskController.js
@@ -1,22 +1,24 @@
 const Task = require("../models/task");
 
 module.exports = class TaskController {
-    static createTask(req, res) {
-        res.render('tasks/create')
-    }
+  static createTask(req, res) {
+    res.render("tasks/create");
+  }
 
-    static async createTaskSave(req, res) {
-        const task = {
-            title: req.body.title,
-            description: req.body.description,
-            done: false,
-        }
-        await Task.create(task);
+  static async createTaskSave(req, res) {
+    const task = {
+      title: req.body.title,
+      description: req.body.description,
+      done: false,
+    };
+    await Task.create(task);
 
-        res.redirect('/tasks')
-    }
-    
-    static showTasks(req, res) {
-        res.render('tasks/all')
-    }
-}
+    res.redirect("/tasks");
+  }
+
+  static async showTasks(req, res) {
+    const tasks = await Task.findAll({ raw: true });
+
+    res.render("tasks/all", { tasks });
+  }
+};
